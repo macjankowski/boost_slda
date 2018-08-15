559f03621f3095863e9b27fa10314cc7b854109c
Maciej Jankowski <mjankowski@pl.imshealth.com>
2018-08-16 00:13:35 +0200

    Implementation of Boost Multi-class sLDA

--- poliblogEstimate_30_iters.sh
+++
@@ -1,4 +0,0 @@
-make clean
-make
-./slda est sample-data/poliblog/shuffled/poliblog.documents.train.txt sample-data/poliblog/shuffled/poliblog.ratings.train.txt settings.txt 0.1 2 seeded results/out_poliblog_ensemble_5-30_topics_30_iters_shuffled
-
